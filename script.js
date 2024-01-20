9510e433153da6727b035d4030bc04b32810450a
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-01-20 17:11:24 +0000

    write function to convert user entry to postfix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let userEntry = '';
+let userEntry = '4+4*2/(1-5)';
 
 function add (numOne, numTwo) {
     return numOne + numTwo;
@@ -22,12 +22,55 @@ function percentOf (numOne, numTwo) {
 }
 
 
-const calculate = {
-    '+': add(a, b),
-    '-': subtract(a, b),
-    '*': multiply(a, b),
-    '/': divide(a, b),
-    '%': percentOf(a, b),
+const oper = {
+    '+': {
+        fn: add,
+        prec: 1,
+    },
+    '-': {
+        fn: subtract,
+        prec: 1,
+    },
+    '*': {
+        fn: multiply,
+        prec: 2,
+    },
+    '/': {
+        fn: divide,
+        prec: 2,
+    },
+'%': percentOf,
+};
+
+
+function shuntingYard (infixExpr) {
+    const expr = infixExpr.split('');
+    const output = [];
+    const operators = []; 
+    expr.forEach(char => {
+        if(isNaN(char)){
+            if(char === '(' || !operators.length || operators[operators.length-1] === '('){
+                operators.push(char);
+            } else if(char !== ')' && operators.length){
+                (oper[char].prec > oper[operators[operators.length-1]].prec)
+                ? operators.push(char)
+                : (output.push(operators.pop()), operators.push(char));
+            } else {
+                output.push(operators.pop());
+                operators.pop();
+            }
+        }else {
+            output.push(char);
+        }
+        console.log(`out: ${output}`);
+        console.log(`oper: ${operators}`);
+    });
+
+    while (operators.length) {
+    output.push(operators.pop());
+    };
+
+    console.log(output);
 };
 
 
