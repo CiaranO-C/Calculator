4e168f4a58c2e9ba89004b58405ec0561b76ac1e
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-01-21 17:32:46 +0000

    rework infix to postfix logic, didn't reorder characters properly.
    some minor styling changes

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-let userEntry = '3+4*2/(1-5)';
+
+let userEntry = document.querySelector('#display');
+const btnContainer = document.querySelector('#button-container');
+const operBtns = document.querySelectorAll('.oper');
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', ()=>{
+    const result = shuntingYard(userEntry.textContent);
+    userEntry.textContent = result;
+})
+
+btnContainer.addEventListener('click', button => {
+   if(button.target.id !== 'button-container'){
+    if(button.target.id !== 'equals' && button.target.id !== 'cancel') {
+    userEntry.textContent += button.target.textContent;
+   };
+
+   if(button.target.classList.contains('oper')){
+    operBtns.forEach(btn => btn.style.color = 'white');
+    button.target.style.color = 'orange';
+   } else {
+    operBtns.forEach(btn => btn.style.color = 'white');
+   };
+
+   if(button.target.id === 'cancel') userEntry.textContent = ''; 
+   };
+});
+
+/*const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    if(button.id !== 'equals' && button.id !== 'cancel') {
+       button.addEventListener('click', event =>{userEntry.textContent += event.target.textContent;});
+    };
+
+    if(button.classList.contains('oper')){
+        button.addEventListener('click', event =>{
+            operBtns.forEach(btn => btn.style.color = 'white');
+            event.target.style.color = 'orange'; 
+        });
+    } else operBtns.forEach(btn => btn.style.color = 'white');
+
+    if(button.id === 'cancel') {
+        button.addEventListener('click', () => userEntry.textContent = '');    
+    };
+});*/
+
+
 
 function add(numOne, numTwo) {
-    return numOne + numTwo;
+    const a = Number(numOne);
+    const b = Number(numTwo);
+    const result = (a+b).toString()
+    console.log(result);
+    return result;
 };
 
 function subtract(numOne, numTwo) {
@@ -46,26 +96,45 @@ function shuntingYard(infixExpr) {
     const expr = infixExpr.split('');
     const output = [];
     const stack = [];
-    expr.forEach(char => {
-        if (isNaN(char)) {
-            if (char === '(' || !stack.length || stack[stack.length - 1] === '(') {
-                stack.push(char);
-            } else if (char !== ')' && stack.length) {
-                (oper[char].prec > oper[stack[stack.length - 1]].prec)
-                    ? stack.push(char)
-                    : (output.push(stack.pop()), stack.push(char));
+    let accum = '';
+    //POOOOOO
+    for (let i = 0; i<expr.length; i++){
+        if (isNaN(expr[i])) {
+            if (expr[i] === '(' || !stack.length || stack[stack.length - 1] === '(') {
+                stack.push(expr[i]);
+            } else if (expr[i] !== ')') {
+               while (stack[stack.length-1] !== '(' && stack.length && oper[expr[i]].prec <= oper[stack[stack.length - 1]].prec){
+                    output.push(stack.pop());
+                };
+                stack.push(expr[i]);
             } else {
-                output.push(stack.pop());
+                while(stack[stack.length-1] !== '('){
+                    output.push(stack.pop());
+                };
                 stack.pop();
             }
         } else {
-            output.push(char);
-        }
-    });
+            //if next char is number, add to accumulate
+            if(!isNaN(expr[i+1])){
+                accum += expr[i];
+            } else {
+                //next char is oper, check if last digit else single digit num
+                if(accum){
+                    accum+=expr[i];
+                    output.push(accum);
+                    accum = '';
+                } else output.push(expr[i]);
+            };
+        };
+        console.log(`outp(${i+1}) ${output}`);
+        console.log(`stak(${i+1}) ${stack}`);
+    };
 
     while (stack.length) {
         output.push(stack.pop());
     };
+
+    console.log(output.join('')); //postfix expression
 
     output.forEach(char => {
         if (!isNaN(char)) {
@@ -81,6 +150,7 @@ function shuntingYard(infixExpr) {
         return shuntingYard(stack[0]);
     } else {
     let expression = stack.toString();
+    
     return expression;
     };
 };
